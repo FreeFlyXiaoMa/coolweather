1a858814328753b92f91868bbfa2084dfbfb9602
Tony <tony@gmail.com>
2017-08-31 17:00:28 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.coolweather.android;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/8/30 0030.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
